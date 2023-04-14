f5607957478805b9291f19c2dc861a67a06c03df
Iwona Behnke <behnke.iwona@gmail.com>
2023-04-14 18:08:48 +0200

    Signup component

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import SignUp from '../components/SignUp'
+import Input from '../components/SignUp/Input'
 
 
 export default function Login() {
   return (
     <>
       <SignUp>
-
+        <Input></Input>
       </SignUp>
     </>
   )
