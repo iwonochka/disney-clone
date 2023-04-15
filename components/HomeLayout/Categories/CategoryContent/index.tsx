c9d00c8bc7e246e722942a1d78feb3a3c5456183
Iwona Behnke <behnke.iwona@gmail.com>
2023-04-15 18:57:11 +0200

    Implemented veideos on hover

--- components/HomeLayout/Categories/CategoryContent/index.tsx
+++ components/HomeLayout/Categories/CategoryContent/index.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import HoverVideoPlayer from "react-hover-video-player";
+
 
 interface Props {
   video: string;
@@ -8,7 +11,10 @@ interface Props {
 
 const CategoryContent = ({video, defaultImage, name}: Props) => {
   return (
-    <div>CategoryContent</div>
+    <HoverVideoPlayer
+      videoSrc={video}
+      pausedOverlay={<Image src={defaultImage} alt={name} fill={true} />}
+    />
   )
 }
 
