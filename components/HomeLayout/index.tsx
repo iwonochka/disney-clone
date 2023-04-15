3a592b02a58e17c81d5a0019bf171555169d7462
Iwona Behnke <behnke.iwona@gmail.com>
2023-04-15 13:43:26 +0200

    Empty Home Layout compnent

---
+++ components/HomeLayout/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+function HomeLayout({}: Props) {
+  return (
+    <>
+      {/* SliderBanner */}
+      {/* Categories */}
+      {/* Rows with filtered movies */}
+    </>
+  )
+}
+
+export default HomeLayout
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,36 @@
+import { useSession } from 'next-auth/react';
 import Head from 'next/head'
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import SignUp from '../components/SignUp';
+import HomeLayout from '../components/HomeLayout';
 
 
 export default function Home() {
-  return (
-    <>
-      <h1>Disney+ Clone</h1>
-    </>
-  )
+
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session) {
+      //redirecting to homepage
+      router.push({
+        pathname: "/login",
+      });
+    }
+    return () => {};
+  }, []);
+
+  if (status === "loading") return <p>Loading</p>;
+
+  if (session)
+    return (
+      <>
+        <HomeLayout>
+
+        </HomeLayout>
+      </>
+    )
+
+
 }
