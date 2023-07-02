8c5fcedbcb7428728dd726a57d45dd25beb99122
Iwona Behnke <behnke.iwona@gmail.com>
2023-07-02 14:58:23 +0200

    Added log out from showpage, home link

--- pages/movies/[id].tsx
+++ pages/movies/[id].tsx
@@ -1,20 +1,32 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { MovieListType } from '../../types/MovieList';
 import { ParsedUrlQuery } from 'querystring';
 import ShowpageLayout from '../../components/ShowpageLayout';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 interface Props {
   movieResults: MovieListType;
 }
 
 export default function Showpage({movieResults}: Props) {
-  console.log(movieResults, "id page")
-  return (
-    <ShowpageLayout movieResults = {movieResults}/>
 
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
-  )
+  useEffect(() => {
+    if (!session) {
+      router.push({
+        pathname: "/login",
+      });
+    }
+    return () => {};
+  }, []);
+
+  if (status === "loading") return <p>Loading</p>;
+
+  if (session) return <ShowpageLayout movieResults={movieResults} />;
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
